9a7afc188865c6a014c495d83fc6906439d6c203
rdesfonds-campus <romain.desfonds@le-campus-numerique.fr>
2026-03-30 18:44:44 +0200

    HeroFactory - choix d'arme et de défense à la création du personnage

--- src/game/Game.java
+++ src/game/Game.java
@@ -1,19 +1,16 @@
 package game;
 
-import characters.Warrior;
-import characters.Wizard;
-import equipment.OffensiveEquipment;
-import equipment.DefensiveEquipment;
-
 public class Game {
 
     private Menu menu;
     private characters.Character currentCharacter;
     private int position;
+    private HeroFactory heroFactory;
 
     public Game(Menu menu) {
         this.menu = menu;
         this.position = 1;
+        this.heroFactory = new HeroFactory(menu);
     }
 
     public void start() {
@@ -49,23 +46,7 @@ public class Game {
     }
 
     private void creerPersonnage() {
-        menu.showMessage("Quel type de personnage veux-tu ?");
-        menu.showMessage("1 - Warrior");
-        menu.showMessage("2 - Wizard");
-
-        int type = menu.askInt("Ton choix :");
-        String nom = menu.askString("Quel est le nom de ton personnage ?");
-
-        if (type == 1) {
-            OffensiveEquipment arme = new OffensiveEquipment("Weapon", 3, "Épée rouillée");
-            DefensiveEquipment bouclier = new DefensiveEquipment("Shield", 2, "Petit bouclier");
-            currentCharacter = new Warrior(nom, 10, 5, arme, bouclier);
-        } else {
-            OffensiveEquipment sort = new OffensiveEquipment("Spell", 4, "Boule de feu");
-            DefensiveEquipment potion = new DefensiveEquipment("Potion", 3, "Petite potion");
-            currentCharacter = new Wizard(nom, 6, 8, sort, potion);
-        }
-
+        currentCharacter = heroFactory.creerPersonnage();
         menu.showMessage("Personnage créé !");
         menu.showMessage(currentCharacter.toString());
     }
---
+++ src/game/HeroFactory.java
@@ -0,0 +1,83 @@
+package game;
+
+import characters.Character;
+import characters.Warrior;
+import characters.Wizard;
+import equipment.DefensiveEquipment;
+import equipment.OffensiveEquipment;
+
+public class HeroFactory {
+
+    private Menu menu;
+
+    public HeroFactory(Menu menu) {
+        this.menu = menu;
+    }
+
+    public characters.Character creerPersonnage() {
+        menu.showMessage("Quel type de personnage veux-tu ?");
+        menu.showMessage("1 - Warrior");
+        menu.showMessage("2 - Wizard");
+        int type = menu.askInt("Ton choix :");
+
+        String nom = menu.askString("Quel est le nom de ton personnage ?");
+
+        if (type == 1) {
+            return creerWarrior(nom);
+        } else {
+            return creerWizard(nom);
+        }
+    }
+
+    private characters.Character creerWarrior(String nom) {
+        menu.showMessage("Choisis ton arme :");
+        menu.showMessage("1 - Épée rouillée (Attaque : 3)");
+        menu.showMessage("2 - Hache de guerre (Attaque : 5)");
+        menu.showMessage("3 - Dague (Attaque : 2)");
+        int choixArme = menu.askInt("Ton choix :");
+
+        OffensiveEquipment arme;
+        switch (choixArme) {
+            case 2:  arme = new OffensiveEquipment("Weapon", 5, "Hache de guerre"); break;
+            case 3:  arme = new OffensiveEquipment("Weapon", 2, "Dague"); break;
+            default: arme = new OffensiveEquipment("Weapon", 3, "Épée rouillée"); break;
+        }
+
+        menu.showMessage("Choisis ta défense :");
+        menu.showMessage("1 - Petit bouclier (Défense : 2)");
+        menu.showMessage("2 - Bouclier de fer (Défense : 4)");
+        int choixDef = menu.askInt("Ton choix :");
+
+        DefensiveEquipment defense = (choixDef == 2)
+                ? new DefensiveEquipment("Shield", 4, "Bouclier de fer")
+                : new DefensiveEquipment("Shield", 2, "Petit bouclier");
+
+        return new Warrior(nom, 10, 5, arme, defense);
+    }
+
+    private characters.Character creerWizard(String nom) {
+        menu.showMessage("Choisis ton sort :");
+        menu.showMessage("1 - Boule de feu (Attaque : 4)");
+        menu.showMessage("2 - Éclair (Attaque : 6)");
+        menu.showMessage("3 - Rayon de givre (Attaque : 3)");
+        int choixSort = menu.askInt("Ton choix :");
+
+        OffensiveEquipment arme;
+        switch (choixSort) {
+            case 2:  arme = new OffensiveEquipment("Spell", 6, "Éclair"); break;
+            case 3:  arme = new OffensiveEquipment("Spell", 3, "Rayon de givre"); break;
+            default: arme = new OffensiveEquipment("Spell", 4, "Boule de feu"); break;
+        }
+
+        menu.showMessage("Choisis ta défense :");
+        menu.showMessage("1 - Petite potion (Défense : 3)");
+        menu.showMessage("2 - Grande potion (Défense : 5)");
+        int choixDef = menu.askInt("Ton choix :");
+
+        DefensiveEquipment defense = (choixDef == 2)
+                ? new DefensiveEquipment("Potion", 5, "Grande potion")
+                : new DefensiveEquipment("Potion", 3, "Petite potion");
+
+        return new Wizard(nom, 6, 8, arme, defense);
+    }
+}
